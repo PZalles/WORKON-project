5c055e8a9ff7592ea1bf57db60e1a59d201b82ff
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-09-25 22:22:11 +0200

    Add css code for nav

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 $('nav ul li a').on('click', function () {
     const goToSection = "[data-section=" + $(this).attr('class') + "]";
     $('body, html').animate({
-        scrollTop: $(goToSection).offset().top
-    })
+        scrollTop: $(goToSection).offset().top - 80
+    });
+})
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
 })
