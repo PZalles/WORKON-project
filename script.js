bf9a26466b3777bb453929d70b73485e79a035f3
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-09-26 10:37:02 +0200

    Add animation for nav

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ $('nav ul li a').on('click', function () {
 $(function () {
     $(document).scroll(function () {
         var $nav = $("nav");
+        var $img = $("nav ul li img");
+        // var $height = $("nav ul li a");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $img.toggleClass('changeSize', $(this).scrollTop() > $nav.height());
+        // $height.toggleClass('lineHeight', $(this).scrollTop() > $nav.height());
     });
 })
