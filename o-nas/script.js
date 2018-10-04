d8b54fc158fd8e5556a74185e778f567068f0e5a
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-10-04 13:41:16 +0200

    Update o nas subpage

--- o-nas/script.js
+++ o-nas/script.js
@@ -8,14 +8,14 @@ const h1Html = document.querySelector('.member h1');
 const h2Html = document.querySelector('.member h2');
 
 // Informacje w tablicach o elementach podmienianych
-const colorImages = ['i1.png', 'i2.png', 'i3.png'];
-const grayImages = ['i1a.png', 'i2a.png', 'i3a.png'];
-const names = ['Przemysław Zalewski', 'Iga Sylwestrowicz', 'Damian Woźniak'];
-const professions = ['Front-end developer', 'Back-end developer', 'Content manager'];
+const colorImages = ['i1.png', 'i2.png', 'i3.png', 'i4.png'];
+const grayImages = ['i1a.png', 'i2a.png', 'i3a.png', 'i4a.png'];
+const names = ['Przemysław Zalewski', 'Iga Sylwestrowicz', 'Damian Woźniak', 'Magdalena Włodarczyk'];
+const professions = ['Front-end developer', 'Front-end developer', 'Front-end developer', 'Front-end developer'];
 
 function changeElement() {
     activeElement++;
-    if (activeElement == 3) {
+    if (activeElement == 4) {
         activeElement = 0;
     }
     colorImgHtml.src = colorImages[activeElement];
