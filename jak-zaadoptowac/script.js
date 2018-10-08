a9905651e5dfa6049d05ac0f1492613f43c3726b
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-10-08 21:03:14 +0200

    Update changes with subpages

---
+++ jak-zaadoptowac/script.js
@@ -0,0 +1,17 @@
+$('nav ul li a').on('click', function () {
+    const goToSection = "[data-section=" + $(this).attr('class') + "]";
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top - 80
+    });
+})
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("nav");
+        var $img = $("nav ul li img");
+        var $height = $("nav ul li");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $img.toggleClass('changeSize', $(this).scrollTop() > $nav.height());
+        $height.toggleClass('lineHeight', $(this).scrollTop() > $nav.height());
+    });
+})
