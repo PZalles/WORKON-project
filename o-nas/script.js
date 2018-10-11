e3f566ec96a8f75e77cb5c1d130e54d4ca4db343
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-10-11 07:28:43 +0200

    Update project

--- o-nas/script.js
+++ o-nas/script.js
@@ -6,12 +6,14 @@ const colorImgHtml = document.querySelector('.color');
 const grayImgHtml = document.querySelector('.gray');
 const h1Html = document.querySelector('.member h1');
 const h2Html = document.querySelector('.member h2');
+const cityHtml = document.querySelector('.member h3');
 
 // Informacje w tablicach o elementach podmienianych
 const colorImages = ['i1.png', 'i2.png', 'i3.png', 'i4.png'];
 const grayImages = ['i1a.png', 'i2a.png', 'i3a.png', 'i4a.png'];
 const names = ['Przemysław Zalewski', 'Iga Sylwestrowicz', 'Damian Woźniak', 'Magdalena Włodarczyk'];
-const professions = ['Front-end developer', 'Front-end developer', 'Front-end developer', 'Front-end developer'];
+const professions = ['Front-end Developer', 'Front-end Developer', 'Front-end developer', 'Grafik'];
+const city = ['Toruń', 'Toruń', 'Toruń', 'Bydgoszcz'];
 
 function changeElement() {
     activeElement++;
@@ -22,6 +24,7 @@ function changeElement() {
     grayImgHtml.src = grayImages[activeElement];
     h1Html.textContent = names[activeElement];
     h2Html.textContent = professions[activeElement];
+    cityHtml.textContent = city[activeElement];
 }
 
 setInterval(changeElement, 8000)
